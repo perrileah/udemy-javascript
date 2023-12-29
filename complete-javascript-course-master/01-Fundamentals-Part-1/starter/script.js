701f1c05b9e85494e1f9dbc23ffc025262bd6fff
Leah Perri <perrileah2023@gmail.com>
2023-12-29 13:17:40 -0600

    data types

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,26 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-let firstName = "Jonas";
+// let firstName = "Jonas";
 
-console.log(firstName);
+// console.log(firstName);
+
+let javascriptIsFun = true;
+
+console.log(typeof javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof 23);
+
+javascriptIsFun = "YES!"; //change value of already existing variable. 'let' is not needed
+
+console.log(typeof javascriptIsFun);
+
+let year; //undefined empty variable
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); // javascript error! null is not an object. null is null.
