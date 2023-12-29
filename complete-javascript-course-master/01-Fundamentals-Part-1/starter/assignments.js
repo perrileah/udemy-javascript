2c27d4866b72955fb7d7ffd603f14aa46dba13f7
Leah Perri <perrileah2023@gmail.com>
2023-12-29 12:54:05 -0600

    practice assignments

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = "United States of America";
+let continent = "North America";
+let population = "331.9 million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
