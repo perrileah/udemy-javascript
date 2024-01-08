4d79a9ff06bee0d8112a125109b14e87c76b59c1
Leah Perri <perrileah2023@gmail.com>
2024-01-08 10:41:20 -0600

    template literals coding challenge 2

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/coding_challenges.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/coding_challenges.js
@@ -9,6 +9,27 @@
 // 3. CreateaBooleanvariable'markHigherBMI'containinginformationabout
 // whether Mark has a higher BMI than John.
 
+// let massMark = 78;
+// let heightMark = 1.69;
+// let massJohn = 92;
+// let heightJohn = 1.95;
+
+// let BMIMark = massMark / (heightMark * heightMark);
+
+// let BMIJohn = massJohn / (heightJohn * heightJohn);
+
+// console.log(BMIMark, BMIJohn);
+
+// if (BMIMark > BMIJohn) {
+//   console.log(true);
+// }
+
+// Coding Challenge #2
+// Use the BMI example from Challenge #1, and the code you already wrote, and improve it.
+// Your tasks:
+// 1. Printaniceoutputtotheconsole,sayingwhohasthehigherBMI.Themessage is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+// 2. UseatemplateliteraltoincludetheBMIvaluesintheoutputs.Example:"Mark's BMI (28.3) is higher than John's (23.9)!"
+
 let massMark = 78;
 let heightMark = 1.69;
 let massJohn = 92;
@@ -18,8 +39,8 @@ let BMIMark = massMark / (heightMark * heightMark);
 
 let BMIJohn = massJohn / (heightJohn * heightJohn);
 
-console.log(BMIMark, BMIJohn);
-
 if (BMIMark > BMIJohn) {
-  console.log(true);
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+} else {
+  console.log(`John's BMI is higher than Mark's!`);
 }
