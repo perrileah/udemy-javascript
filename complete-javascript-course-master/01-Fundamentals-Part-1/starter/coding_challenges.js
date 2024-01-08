4c775ae72041cf46ff0ed03b3aa38e0e357f6ca5
Leah Perri <perrileah2023@gmail.com>
2024-01-08 10:11:56 -0600

    coding challenge 1

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/coding_challenges.js
@@ -0,0 +1,25 @@
+// Coding Challenge #1
+
+// Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula:
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
+// Your tasks:
+// 1. StoreMark'sandJohn'smassandheightinvariables
+// 2. CalculateboththeirBMIsusingtheformula(youcanevenimplementboth
+// versions)
+// 3. CreateaBooleanvariable'markHigherBMI'containinginformationabout
+// whether Mark has a higher BMI than John.
+
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJohn = 1.95;
+
+let BMIMark = massMark / (heightMark * heightMark);
+
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+  console.log(true);
+}
--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -37,28 +37,26 @@
 // job = "teacher";
 
 // math operators
-cont now = 2037
+// cont now = 2037
 
-const ageJonas = now - 1991
-const ageSarah = now - 2018
-console.log(ageJonas, ageSarah);
+// const ageJonas = now - 1991
+// const ageSarah = now - 2018
+// console.log(ageJonas, ageSarah);
 
-console.log(ageJonas * 2, ageJonas / 2, 2 ** 3)
-// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+// console.log(ageJonas * 2, ageJonas / 2, 2 ** 3)
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
 
-const firstName = 'Jonas';
-const lastName = 'Perri';
-console.log(firstName + ' ' + lastName);
+// const firstName = 'Jonas';
+// const lastName = 'Perri';
+// console.log(firstName + ' ' + lastName);
 
-//asignment operators
-let x = 10 + 5; // equal sign is an assignment operator. // 15
+// //asignment operators
+// let x = 10 + 5; // equal sign is an assignment operator. // 15
 
-x += 10; // x = x + 10 = 25
-x *= 4; // x = x * 4 = 100
-x++; // x = x + 1
-x--; // x = x - 1
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1
+// x--; // x = x - 1
 
-
-//comparison operators 
-console.log(ageJonas > ageSarah) //replies with boolean
-
+// //comparison operators
+// console.log(ageJonas > ageSarah) //replies with boolean
